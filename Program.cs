487c75994998e6aa7371f349fe080f19d5122f58
siyuexiong <2498951033@qq.com>
2020-09-21 17:45:34 +0800

    第二次提交

--- Program.cs
+++ Program.cs
@@ -6,8 +6,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
             Console.WriteLine("this is my first line");
+            Console.WriteLine("this is my second line");
         }
     }
 }
